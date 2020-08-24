e2c1e5665d1633543c5ca5f50d361c9b7e55b921
miaonster <miaonster17@gmail.com>
2020-08-24 13:44:32 +0800

    feat: add Barcode & QRCode custom style

--- packages/taro-code/src/components/Barcode/index.tsx
+++ packages/taro-code/src/components/Barcode/index.tsx
@@ -3,19 +3,20 @@ import PropTypes from 'prop-types'
 import { Image } from '@tarojs/components'
 import barcode from '../../common/barcode'
 
-const Barcode = ({ text = '', scale = 4, width = 300, height = 60 }) => {
+const Barcode = ({ text = '', scale = 4, width = 300, height = 60, style = {} }) => {
   const image = useMemo(() => barcode({ text, scale }), [text, scale])
   const widthString = width ? width + 'px' : ''
   const heightString = height ? height + 'px' : ''
-  const style = { width: widthString, height: heightString }
-  return <Image style={style} src={image || ''} />
+  const finalStyle = { width: widthString, height: heightString, ...style }
+  return <Image style={finalStyle} src={image || ''} />
 }
 
 Barcode.propTypes = {
   text: PropTypes.string.isRequired,
   scale: PropTypes.number,
   width: PropTypes.number,
-  height: PropTypes.number
+  height: PropTypes.number,
+  style: PropTypes.object,
 }
 
 export default Barcode
--- packages/taro-code/src/components/QRCode/index.tsx
+++ packages/taro-code/src/components/QRCode/index.tsx
@@ -3,19 +3,22 @@ import PropTypes from 'prop-types'
 import { Image } from '@tarojs/components'
 import { createQrCodeImg } from '../../common/qrcode'
 
-const QRCode = ({ text = '', size = 100, scale = 4, typeNumber = 2, errorCorrectLevel = 'M' }) => {
+const QRCode = ({ text = '', size = 100, scale = 4, typeNumber = 2, errorCorrectLevel = 'M', style = {} }) => {
   const image = useMemo(() => {
     const options = { errorCorrectLevel, typeNumber, size: size * scale }
     return createQrCodeImg(text, options)
   }, [text, scale, size, errorCorrectLevel, typeNumber])
-  const style = { width: size + 'px', height: size + 'px' }
-  return <Image style={style} src={image} />
+  const widthString = size ? size + 'px' : ''
+  const heightString = size ? size + 'px' : ''
+  const finalStyle = { width: widthString, height: heightString, ...style }
+  return <Image style={finalStyle} src={image} />
 }
 
 QRCode.propTypes = {
   text: PropTypes.string.isRequired,
   size: PropTypes.number,
   scale: PropTypes.number,
+  style: PropTypes.object,
   errorCorrectLevel: PropTypes.string,
   typeNumber: PropTypes.number
 }
